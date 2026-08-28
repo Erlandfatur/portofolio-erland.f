fbfa037fdc65719ba83199e8bf59dae6f0d3a342
Erlandfatur <erlandinfo@gmail.com>
2026-08-28 17:20:32 +0700

    ci: add GitHub Pages automated deployment workflow and base config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/portofolio-erland.f/',
   plugins: [
     react(),
     tailwindcss()
